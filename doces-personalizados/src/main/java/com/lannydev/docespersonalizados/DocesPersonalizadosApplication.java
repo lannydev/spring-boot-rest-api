67cdd1342cf35723200d24455999915362a53849
lannydev <e.cristina00@gmai.com>
2022-11-26 14:54:11 -0300

    Criando projeto Doces personalizados

---
+++ doces-personalizados/src/main/java/com/lannydev/docespersonalizados/DocesPersonalizadosApplication.java
@@ -0,0 +1,13 @@
+package com.lannydev.docespersonalizados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocesPersonalizadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocesPersonalizadosApplication.class, args);
+	}
+
+}
---
+++ doces-personalizados/src/test/java/com/lannydev/docespersonalizados/DocesPersonalizadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lannydev.docespersonalizados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocesPersonalizadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
