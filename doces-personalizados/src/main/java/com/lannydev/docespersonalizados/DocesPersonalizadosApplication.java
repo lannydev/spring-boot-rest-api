cb383641efb2c1a3b6e85d521be06c35ec2337d5
lannydev <e.cristina00@gmai.com>
2022-11-26 15:53:42 -0300

    Hello world com Spring Boot

--- doces-personalizados/src/main/java/com/lannydev/docespersonalizados/DocesPersonalizadosApplication.java
+++ doces-personalizados/src/main/java/com/lannydev/docespersonalizados/DocesPersonalizadosApplication.java
@@ -2,9 +2,16 @@ package com.lannydev.docespersonalizados;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DocesPersonalizadosApplication {
+	@GetMapping("/hello")
+	public String helloWorld(){
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DocesPersonalizadosApplication.class, args);
