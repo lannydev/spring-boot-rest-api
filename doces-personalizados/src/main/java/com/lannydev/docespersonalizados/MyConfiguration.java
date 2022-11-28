fd28466e843e7549879d9f1caf6d03a852ae37ef
lannydev <e.cristina00@gmai.com>
2022-11-28 01:24:17 -0300

    Trabalhando com multiplos ambientes e profiles

--- doces-personalizados/src/main/java/com/lannydev/docespersonalizados/MyConfiguration.java
+++ doces-personalizados/src/main/java/com/lannydev/docespersonalizados/MyConfiguration.java
@@ -1,9 +1,18 @@
 package com.lannydev.docespersonalizados;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MyConfiguration {
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("Rodando o ambiente de Desenvolvimento");
+        };
+    }
 
 }
